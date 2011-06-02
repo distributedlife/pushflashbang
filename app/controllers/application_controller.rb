8c1418677e4690fd4d679e02d289009afe2a01d5
Ryan Boucher <ryan.boucher@distributedlife.com>
2011-06-02 13:06:32 +1000

    trying style for mobile browsers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,33 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  layout :detect_browser
 
   private
+  MOBILE_BROWSERS = ['android']
+
   def after_sign_in_path_for(resource_or_scope)
     user_index_path
+  end
+
+  def detect_browser
+    layout = selected_layout
+    return layout if layout
+
+    agent = request.headers["HTTP_USER_AGENT"].downcase
+
+    MOBILE_BROWSERS.each do |m|
+      return 'mobile_application' if agent.match(m)
+    end
+
+    return 'application'
+  end
+
+  def selected_layout
+    session.inspect # force session load
+    if session.has_key? "layout"
+      return (session["layout"] == "mobile") ? "mobile_application" : "application"
+    end
+
+    return nil
   end
 end
