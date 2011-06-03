4009c938b825eebea29bad9973d81361299f394d
Ryan Boucher <ryan.boucher@distributedlife.com>
2011-06-03 23:20:44 +1000

    mobile style testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,8 @@ class ApplicationController < ActionController::Base
       return 'mobile_application' if agent.match(m)
     end
 
-    return 'application'
+#    return 'application'
+    return 'mobile_application'
   end
 
   def selected_layout
