e64049d4029039bb81df3717e803273e04e92945
Ryan Boucher <ryan.boucher@distributedlife.com>
2011-07-26 13:21:25 +1000

    migrate position code from migration

--- db/migrate/20110724093102_add_position_to_set_terms.rb
+++ db/migrate/20110724093102_add_position_to_set_terms.rb
@@ -1,18 +1,6 @@
 class AddPositionToSetTerms < ActiveRecord::Migration
   def self.up
     add_column :set_terms, :position, :integer
-
-    current_set_term = 0
-    current_position = 1
-    SetTerms.group(:set_id).all.each do |set_term|
-      if current_set_term != set_term.set_id
-        current_position = 1
-        current_set_term = set_term.set_id
-      end
-
-      set_term.position = current_position
-      current_position = current_position + 1
-    end
   end
 
   def self.down
