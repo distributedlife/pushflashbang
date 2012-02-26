9dbb0911ff2128ca04cc12fba9a780229291b3c1
Ryan Boucher <ryan.boucher@distributedlife.com>
2012-02-26 19:36:00 +1100

    setting compile assets to true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ PushFlashBang::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
