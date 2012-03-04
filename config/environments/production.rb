f6cffc3f594adf505b19c86bd6627732a5830edd
Ryan Boucher <ryan.boucher@distributedlife.com>
2012-03-04 21:55:51 +1100

    disable asset compilation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,7 @@ PushFlashBang::Application.configure do
   # In production, Apache or nginx will already do this
   config.serve_static_assets = true
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  config.assets.compress = false
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
 
